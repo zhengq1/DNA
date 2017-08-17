f9459760014f30ce666e82065de4b64a1031ace1
zhengq1 <ljqlyy@gmail.com>
2017-08-17 17:27:13 +0800

    NewIdentityUpdateTransaction
    Signed-off-by: zhengq1 <ljqlyy@gmail.com>

--- core/transaction/TransactionBuilder.go
+++ core/transaction/TransactionBuilder.go
@@ -142,3 +142,19 @@ func NewDataFileTransaction(path string, fileName string, note string, issuer *c
 		Programs:      []*program.Program{},
 	}, nil
 }
+
+func NewIdentityUpdateTransaction(pubKey *crypto.PubKey, did []byte, ddo []byte) (*Transaction, error) {
+	payload := &payload.IdentityUpdate{
+		DID:     did,
+		DDO:     ddo,
+		Updater: pubKey,
+	}
+	return &Transaction{
+		TxType:        IdentityUpdate,
+		Payload:       payload,
+		Attributes:    []*TxAttribute{},
+		UTXOInputs:    []*UTXOTxInput{},
+		BalanceInputs: []*BalanceTxInput{},
+		Programs:      []*program.Program{},
+	}, nil
+}
